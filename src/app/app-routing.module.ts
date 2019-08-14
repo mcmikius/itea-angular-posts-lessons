51b8babf811caccea4eb1b8792d290e9dd694637
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-14 10:59:33 +0300

    fix routerLink
    Signed-off-by: Mykhailo <mcmikius@icloud.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {PostsListComponent} from './posts-list/posts-list.component';
 import {PostComponent} from './post/post.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  {
-    path: '', pathMatch: 'full', redirectTo: 'posts'
-  },
-  {
-    path: 'posts',
-    component: PostsListComponent
-  },
-  {
-    path: 'posts/:id',
-    component: PostComponent
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+  {path: 'posts/:id', component: PostComponent},
+  {path: 'posts', component: PostsListComponent},
+  {path: '', pathMatch: 'full', redirectTo: '/posts'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
